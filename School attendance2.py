98a918f5aef42b910138d487fa3ae5cde6864b2c
Vera KAYMAZ <129058895+Vera-Kaymaz@users.noreply.github.com>
2023-04-05 12:14:07 +0300

    School attendance

---
+++ School attendance2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+student = "ill"
+if student == True:
+  print("The student is at school")
+    
+elif student == False:
+  print("The student is not at school")
+    
+else:
+  print("The student is on leave")
+
